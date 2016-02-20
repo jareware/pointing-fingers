6b7064e0a6b59274ec7aa09ce4b14f6827506fbf
Jarno Rantanen <jarno@jrw.fi>
2016-02-20 14:21:46 +0200

    Add first working test case.

--- index.js
+++ index.js
@@ -1,7 +1,73 @@
+var axios = require('axios');
+var fs = require('fs');
+var _ = require('lodash');
+var stringify = require('json-stable-stringify');
+
 module.exports = {
-  checkUpstreamApis: checkUpstreamApis
+  testUpstreamChanges: testUpstreamChanges
 };
 
-function checkUpstreamApis(options) {
-  return options;
+function toString(x) {
+  return stringify(x, { space: '    ' });
+}
+
+function defaultRunner(description, callback) {
+  return callback();
+}
+
+function defaultAssert(actual, expected) {
+  var act = toString(actual);
+  if (act !== toString(expected)) {
+    throw new Error('Fixture mismatch, actual: ' + act);
+  }
+}
+
+function testUpstreamChanges(opt) {
+
+  return Promise.all((opt.urls || []).map(function(url) {
+
+    return (opt.runner || defaultRunner)(url, function() {
+      return axios({
+        url: (opt.base || '') + url,
+        method: opt.method || 'GET',
+        headers: opt.headers || {}
+      }).then(test, test);
+    });
+
+    function test(res) {
+
+      var actual = _.omit(res, 'config');
+
+      (opt.ignores || []).forEach(function(ignore) {
+        if (_.has(actual, ignore)) {
+          _.set(actual, ignore, opt.placeholder || null);
+        }
+      });
+
+      (opt.transforms || []).forEach(function(transform) {
+        try {
+          transform(res);
+        } catch (e) {}
+      });
+
+      if (opt.learn) {
+        fs.writeFileSync(filename(url), toString(actual));
+      } else {
+        var expected = JSON.parse(fs.readFileSync(filename(url)));
+        (opt.assert || defaultAssert)(actual, expected);
+      }
+
+    }
+
+    function filename(url) {
+      return (opt.fixtures || '/dev/null/')
+        + url
+          .replace(/^\//, '')
+          .replace(/[^a-zA-Z0-9]/g, '_')
+          .replace(/_+/g, '_')
+        + '.json';
+    }
+
+  }));
+
 }
--- index.spec.js
+++ index.spec.js
@@ -1,10 +1,19 @@
 const assert = require('chai').assert; // @see http://chaijs.com/api/assert/
-const checkUpstreamApis = require('./index').checkUpstreamApis;
+const testUpstreamChanges = require('./index').testUpstreamChanges;
 
 describe('pointing-fingers', () => {
 
-  it('works', () => {
-    assert.deepEqual(checkUpstreamApis({}), {}); // TODO
+  it('works without mocha integration', () => {
+    return testUpstreamChanges({
+      learn: false,
+      fixtures: 'fixtures/',
+      urls: [
+        'https://api.github.com/user'
+      ],
+      ignores: [
+        'headers'
+      ]
+    });
   });
 
 });
